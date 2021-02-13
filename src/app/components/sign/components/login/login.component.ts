c32a95e910929733ff07d0f3d374e1ab53ba59a5
bartekrozycki <bartek.rozycki@icloud.com>
2021-02-13 13:25:52 +0100

    move

--- src/app/components/sign/components/login/login.component.ts
+++ src/app/components/sign/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import {AuthService} from '../../services/auth-service.service';
+import {AuthService} from '../../../../services/auth-service.service';
 import {Router} from '@angular/router';
 
 @Component({
--- src/app/components/sign/components/register/register.component.ts
+++ src/app/components/sign/components/register/register.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {AuthService} from '../../services/auth-service.service';
+import {AuthService} from '../../../../services/auth-service.service';
 import {Router} from '@angular/router';
 
 function passwordMatchValidator(frm: FormGroup): { mismatch: boolean } {
